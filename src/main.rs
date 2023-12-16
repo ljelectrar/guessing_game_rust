a8ffb6e65a2130dfeba11069a1efa45950a93822
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-16 09:46:47 -0300

    Get a random number using rand and add dependecies.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
     //processing a guess
     println!("Guess the number, baby!");
