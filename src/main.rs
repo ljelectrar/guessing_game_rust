bfe84cdff87482f07d3e3b293e84c813483be090
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-16 09:16:01 -0300

    Processing user guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    //processing a guess
+    println!("Guess the number, baby!");
+    println!("Please, input using the keyboard your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guess: {guess}");
+
 }
